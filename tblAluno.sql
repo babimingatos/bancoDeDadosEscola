b4dcc9833d4cd145684cbf93a3f8b758c1597c13
Barbara Mingatos <72284498+babimingatos@users.noreply.github.com>
2021-06-29 13:41:37 -0300

    Create tblAluno.sql

---
+++ tblAluno.sql
@@ -0,0 +1,10 @@
+CREATE TABLE tblaluno (
+RMAluno int PRIMARY key NOT null AUTO_INCREMENT, 
+nomeCompletoAluno varchar (255) NOT null, 
+cpfAluno int (11) NOT null, 
+rgAluno varchar (9) NOT null, 
+emailAluno varchar (45) NOT null, 
+senhaEmailAluno varchar (45) NOT null, 
+celularAluno BIGINT (11) NOT null,
+telefoneAluno varchar (45) 
+);
